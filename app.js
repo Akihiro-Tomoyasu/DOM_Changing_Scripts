dfd05dde4d46f9037fc1dc13dd513d925d02cb36
Akihiro-Tomoyasu <30279753+Akihiro-Tomoyasu@users.noreply.github.com>
2018-10-04 20:27:52 +0900

    add script
    要素のwidth,height,y,xを取得
    サイドコンテンツを追従させる

--- app.js
+++ app.js
@@ -37,3 +37,70 @@ var randomElements = function(selectors){
 	}
 	return arr;
 };
+
+
+
+/**
+ * 要素のwidth,height,y,xを取得
+**/
+var getElementRects = function(element){
+	if(element.length === null) return false;
+	
+	var rect = element.getBoundingClientRect();
+	var rects = {};
+	rects['w'] = rect.width;
+	rects['h'] = rect.height;
+	rects['x'] = rect.left + window.pageXOffset
+	rects['y'] = rect.top + window.pageYOffset;
+	rects['wx'] = rects['w'] + rects['x'];
+	rects['hy'] = rects['h'] + rects['y'];
+	return rects;
+};
+
+
+
+/**
+ * サイドコンテンツを追従させる
+**/
+var beforeY,nowY;
+var stickySideContent = function(){
+	//要素取得
+	var mainElement = document.querySelector('#index-main,#search-results-main');
+	var sideElement = document.querySelector('aside.widgets');
+	var sideInnerElement = document.querySelector('#aside-inner');
+	var headerElement = document.querySelector('#header');
+	
+	//要素がなければ終了
+	if(mainElement === null || sideElement.length === null || sideInnerElement.length === null) return false;
+	
+	//要素からSize、Position取得
+	var mainRects = getElementRects(mainElement);
+	var sideRects = getElementRects(sideElement);
+	var sideInnerRects = getElementRects(sideInnerElement);
+	var headerRects = getElementRects(headerElement);
+	
+	//メインがサイドインナーより低ければ終了
+	if(mainRects.h < sideInnerRects.h) return false;
+	
+	//スクロール量取得
+	beforeY = nowY === undefined ? window.scrollY :nowY;
+	nowY = window.scrollY;
+	
+	//windowサイズ
+	w = window.innerWidth;
+	h = window.innerHeight;
+	
+	//スクロール向き判定
+	var scrollMoving = nowY >= beforeY ? 'down' : 'up';
+	
+	//sideInnerElement.style.transition = '0.5s';
+	if(mainRects.y > nowY){ //スクロール量がメインの上端を越えたとき
+		sideInnerElement.style.transform = 'translateY(0px)';
+	} else if(mainRects.hy < nowY + h){  //スクロール量がメインの下端を越えたとき
+		sideInnerElement.style.transform = 'translateY(' + (mainRects.hy - sideInnerRects.h  - sideRects.y) + 'px)';
+	} else if(scrollMoving === 'down' && nowY + h < mainRects.hy && nowY + h > sideInnerRects.hy){ //下スクロール
+		sideInnerElement.style.transform = 'translateY(' + (nowY + h - sideRects.hy) + 'px)';
+	} else if(scrollMoving === 'up' && nowY < sideInnerRects.y + headerRects.h && sideRects.y < sideInnerRects.y){ //上スクロール
+		sideInnerElement.style.transform = 'translateY(' + (nowY + headerRects.h - sideRects.y) + 'px)';
+	}
+};
